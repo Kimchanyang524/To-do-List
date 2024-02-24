7cd550a54fd0861beae6cd6a4152a115a625dbe4
KCY <cks402@naver.com>
2024-02-24 23:18:27 +0900

    ✅ UPDATE: todo-index

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class TodoItem(models.Model):
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import TodoItem
 
 
 def index(request):
-    return render(request, "todo/index.html")
+    context = {"todo_items": TodoItem.objects.all()}
+    return render(request, "todo/index.html", context)
+
+
+def add_todo(request):
+    todo = request.POST["todo"]
+    TodoItem(content=todo).save()
+    return redirect("todo-index")
