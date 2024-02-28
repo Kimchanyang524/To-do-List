aa8d491bd835a8e8627a17f2a55affcb0739deac
KCY <cks402@naver.com>
2024-02-28 20:07:23 +0900

    ✅ UPDATE: todo-remove

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="todo-index"),
     path("add-todo/", views.add_todo, name="add-todo"),
+    path("remove-todo/<int:pk>", views.remove_todo, name="remove-todo"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -3,11 +3,16 @@ from .models import TodoItem
 
 
 def index(request):
-    context = {"todo_items": TodoItem.objects.all()}
+    context = {"todoitems": TodoItem.objects.all()}
     return render(request, "todo/index.html", context)
 
 
 def add_todo(request):
     todo = request.POST["todo"]
     TodoItem(content=todo).save()
+    return redirect("todo-index")
+
+
+def remove_todo(request, pk):
+    TodoItem.objects.get(id=pk).delete()
     return redirect("todo-index")
